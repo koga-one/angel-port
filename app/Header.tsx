9b384f6ca54a259f2446887f166f446530e1b804
André Koga <work@koga.one>
2023-03-09 16:14:57 -0500

    did a bunch of stuff :)

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,13 +1,3 @@
-import HeaderButton from "@/components/HeaderButton";
-
 export default function Header() {
-  return (
-    <header className="mb-4">
-      <ul className="flex gap-3 text-lg text-black dark:text-white md:text-xl">
-        <HeaderButton href="/" text="A*K" />
-        <HeaderButton href="/contact" text="Contact" />
-        <HeaderButton href="/resume" text="Resume" classes="ml-auto" />
-      </ul>
-    </header>
-  );
+  return <header></header>;
 }
